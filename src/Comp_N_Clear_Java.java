fcc6661c88e3cf0a6d77887ea633f3803fe458b8
antoniooreany <antonio_oreany@mail.ru>
2016-10-22 01:16:32 +0300

    All the code is written but not tested. I need to sleep.

--- src/Comp_N_Clear_Java.java
+++ src/Comp_N_Clear_Java.java
@@ -1,8 +1,7 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,17 +12,13 @@ public class Comp_N_Clear_Java {
     public static void main(String[] args) throws IOException {
         String srcPath = "D:\\cons_1\\receive\\LAST_REC.TXT";
         String dstPath = "D:\\cons_1\\base";
+        String uVedaPath = "U:\\Veda\\VEDA3000\\CONS_1\\SYSTEM";
 
         HashSet<String> holdNamesArray = ReadRawLinesAndWriteToArray(srcPath);
-//        for (String s:
-//             holdNamesArray) {
-//            System.out.println(s);
-//        }
 
         RemoveUnnecessaryDirectories(holdNamesArray, dstPath);
 
-
-        LaunchKPlus();
+        LaunchKPlus(uVedaPath, dstPath);
     }
 
     private static HashSet<String> ReadRawLinesAndWriteToArray(String srcPath) throws IOException {
@@ -52,17 +47,6 @@ public class Comp_N_Clear_Java {
         HashSet<String> dstSet = getDstDirsSet(dstPath);
         HashSet<String> elemsToDelete = (HashSet<String>) dstSet.clone();
 
-//        for (String s :
-//                dstSet) {
-//            System.out.println(s);
-//        }
-
-        // TODO: 21.10.2016 Remove from dst everything except holdNamesSet, using deleteDirectory(File dir)
-//        HashSet<String> dirsToRemove = getDirsToRemove(holdNamesSet, dstSet);
-//        for (String s :
-//                dirsToRemove) {
-//            System.out.println(s);
-//        }
         for (String dstSetElem :
                 dstSet) {
             for (String holdNamesSetElem :
@@ -83,20 +67,11 @@ public class Comp_N_Clear_Java {
 
     private static HashSet<String> getDirsToRemove(HashSet<String> holdNamesSet, HashSet<String> dstSet) {
         HashSet<String> dirsToRemove = new HashSet<String>();
-//        for (String s :
-//                dstSet) {
-//            System.out.println(s);
-//        }
         for (String holdNamesSetElem :
                 holdNamesSet) {
-//            System.out.println(holdNamesSetElem);
             dstSet.remove(holdNamesSetElem);
         }
 
-//        Iterator iterator = holdNamesSet.iterator();
-//        while (iterator.hasNext()) {
-//            dstSet.remove(iterator.next());
-//        }
 
         dstSet.removeAll(holdNamesSet);
 
@@ -132,7 +107,81 @@ public class Comp_N_Clear_Java {
     }
 
 
-    private static void LaunchKPlus() {
+    private static void LaunchKPlus(String uVedaPath, String dstPath) throws IOException {
         // TODO: 21.10.2016 Realize
+        if (!copyDir(uVedaPath, dstPath)) {
+            throw new FileNotFoundException("Files has not copied from " + uVedaPath + " to " + dstPath);
+        }
+        deleteFile(dstPath + "cd.zip");
+        deleteFile(dstPath + "cef2272.zip");
+
+        ProcessBuilder pb = new ProcessBuilder("D:\\cons_1\\cons.exe");
+        // TODO: 22.10.2016 Maybe I must use not "cons.exe" but "cons.lnk"
+        pb.start();
+    }
+
+    private static boolean deleteFile(String filePath) {
+        File file = new File(filePath);
+        if (file.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private static final int BUFFER_SIZE = 1024;
+
+    private static boolean copyDir(final String src, final String dst) {
+        System.out.println("Копируем каталог: " + src);
+        final File srcFile = new File(src);
+        final File dstFile = new File(dst);
+        if (srcFile.exists() && srcFile.isDirectory() && !dstFile.exists()) {
+            dstFile.mkdir();
+            File nextSrcFile;
+            String nextSrcFilename, nextDstFilename;
+            for (String filename : srcFile.list()) {
+                nextSrcFilename = srcFile.getAbsolutePath()
+                        + File.separator + filename;
+                nextDstFilename = dstFile.getAbsolutePath()
+                        + File.separator + filename;
+                nextSrcFile = new File(nextSrcFilename);
+                if (nextSrcFile.isDirectory()) {
+                    copyDir(nextSrcFilename, nextDstFilename);
+                } else {
+                    copyFile(nextSrcFilename, nextDstFilename);
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean copyFile(final String src, final String dst) {
+        System.out.println("Копируем файл: " + src);
+        final File srcFile = new File(src);
+        final File dstFile = new File(dst);
+        if (srcFile.exists() && srcFile.isFile() && !dstFile.exists()) {
+            try (InputStream in = new FileInputStream(srcFile);
+                 OutputStream out = new FileOutputStream(dstFile)) {
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int bytes;
+                while ((bytes = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, bytes);
+                }
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Comp_N_Clear_Java.class.getName())
+                        .log(Level.SEVERE, null, ex);
+                return false;
+            } catch (IOException ex) {
+                Logger.getLogger(Comp_N_Clear_Java.class.getName())
+                        .log(Level.SEVERE, null, ex);
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 }
