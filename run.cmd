9e8ea8e3cb08051564beb038ad1991ede685c8f0
antoniooreany <antonio_oreany@mail.ru>
2016-10-24 22:40:44 +0300

    run.cmd created.

---
+++ run.cmd
@@ -0,0 +1,8 @@
+REM cd C:\Users\User\IdeaProjects\Comp_N_Clear_Java\out\production\Comp_N_Clear_Java\
+REM C:\Users\User\IdeaProjects\Comp_N_Clear_Java\run.cmd
+REM C:\Users\User\IdeaProjects\Comp_N_Clear_Java\out\production\Comp_N_Clear_Java\HelloWorld.class
+
+cd out\production\Comp_N_Clear_Java\
+java Comp_N_Clear_Java
+
+REM pause
