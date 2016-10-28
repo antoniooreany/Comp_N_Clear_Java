123b861850bc350bb3cd85f05bf077cccb7cee17
Anton Gorshkov <antonio_oreany@mail.ru>
2016-10-28 15:13:13 +0300

    ProcessBuilder pb = new ProcessBuilder("C:\\Users\\gorshkov\\IdeaProjects\\Comp_N_Clear_Java\\out\\production\\Comp_N_Clear_Java\\startConsLnk.cmd");

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) throws IOException {
         ProcessBuilder pb = new ProcessBuilder("startConsLnk_test.cmd");
         System.out.println("Hiiii !!!");
-//        ProcessBuilder pb = new ProcessBuilder("C:\\Users\\gorshkov\\IdeaProjects\\Comp_N_Clear_Java\\startConsLnk.cmd");
+//        ProcessBuilder pb = new ProcessBuilder("C:\\Users\\gorshkov\\IdeaProjects\\Comp_N_Clear_Java\\out\\production\\Comp_N_Clear_Java\\startConsLnk.cmd");
         // 26.10.2016 Maybe I must use not "cons.exe" but "cons.lnk". Done by running "startConsLnk.cmd", which can run "cons.lnk".
         pb.start();
     }
